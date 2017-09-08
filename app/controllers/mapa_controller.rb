efe6dc5232fc40812f8b5aac1465547d550ef046
Jason Tapia Diaz <jas.tapia@bprog.cl>
2017-09-08 00:47:39 -0300

    modelos listos, se añade gon y comentarios

--- app/controllers/mapa_controller.rb
+++ app/controllers/mapa_controller.rb
@@ -1,29 +1,31 @@
 class MapaController < ApplicationController
 	require 'pp'
   def show
+    #se busca el ultimo valor de coordenadas
     @ultimo = Coordinate.last
     gon.latitude = @ultimo.latitude
     gon.longitude = @ultimo.longitude
-
-    p gon.latitude
+    #data por consola
+    pp gon.latitude
   end
 
   def guardar
+    #se busca un vehiculo, si existe se ocupara para guardar la informacion relacionada
+    #sino devuelve nil
   	@vehicle = Vehicle.find_by vehicle_identifier: params[:identificador]
   	puts "latitud #{params[:latitude]} longitud #{params[:longitud]} identifi #{params[:identificador]}"
   	if @vehicle.nil?
-  		puts "hola1"
+      #si el vehiculo no existe se crea
   		Vehicle.create(vehicle_identifier: params[:identificador])
+      #se guardan la latitud y longitud solo si ambos parametros no son vacios
   		if !params[:latitude].blank? && !params[:longitud].blank?
-  			puts "hola2"
   			@vehicle = Vehicle.find_by vehicle_identifier: params[:identificador]
   			puts @vehicle.id
   			Coordinate.create(latitude: params[:latitude], longitude: params[:longitud],vehicle_id: @vehicle.id )
   		end
   	else
-  		puts "hola3"
+       #se guardan la latitud y longitud solo si ambos parametros no son vacios
   		if !params[:latitude].blank? && !params[:longitud].blank?
-  			puts "hola4"
   			@vehicle = Vehicle.find_by vehicle_identifier: params[:identificador]
   			puts @vehicle.id
   			Coordinate.create(latitude: params[:latitude].to_s, longitude: params[:longitud].to_s,vehicle_id: @vehicle.id.to_i )
